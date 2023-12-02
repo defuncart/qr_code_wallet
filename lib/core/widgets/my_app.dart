a633b1c8427ca2666343673d39ac027158832c92
James Leahy <defuncart@gmail.com>
2023-12-02 17:02:59 +0100

    Add loca

--- lib/core/widgets/my_app.dart
+++ lib/core/widgets/my_app.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:qr_code_wallet/core/l10n/l10n_extension.dart';
 import 'package:qr_code_wallet/features/home/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -18,10 +21,17 @@ class MyApp extends StatelessWidget {
     final color = colors[Random().nextInt(colors.length)];
 
     return MaterialApp(
-      title: 'QRCode Wallet',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData.from(colorScheme: ColorScheme.light(primary: color)),
       darkTheme: ThemeData.from(colorScheme: ColorScheme.dark(primary: color)),
+      onGenerateTitle: (context) => context.l10n.appTitle,
       home: const HomePage(),
     );
   }
--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_wallet/core/l10n/l10n_extension.dart';
 import 'package:qr_code_wallet/features/qr_scanner/qr_scanner_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,7 +9,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('QR Code Wallet'),
+        title: Text(context.l10n.appTitle),
       ),
       body: const Center(
         child: Text('Hello World'),
