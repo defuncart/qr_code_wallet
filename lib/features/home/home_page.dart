69c0527bcd416aa2b95c9cce4242f24f07d8d276
James Leahy <defuncart@gmail.com>
2023-12-08 18:15:08 +0100

    Refactor QRTile

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:qr_code_wallet/core/db/models/qr_code.dart';
 import 'package:qr_code_wallet/core/l10n/l10n_extension.dart';
 import 'package:qr_code_wallet/core/state/state.dart';
 import 'package:qr_code_wallet/features/qr_scanner/qr_scanner_widget.dart';
@@ -52,26 +53,58 @@ class HomePageContent extends ConsumerWidget {
             )
           : ListView.builder(
               itemCount: data.value.length,
-              itemBuilder: (context, index) => ListTile(
-                leading: QrImageView(
-                    data: data.value[index].data,
-                    version: QrVersions.auto,
-                    size: 48,
-                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                    eyeStyle: QrEyeStyle(
-                      eyeShape: QrEyeShape.square,
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-                    dataModuleStyle: QrDataModuleStyle(
-                      dataModuleShape: QrDataModuleShape.square,
-                      color: Theme.of(context).colorScheme.onSurface,
-                    )
-                    // errorCorrectionLevel: QrErrorCorrectLevel.H,
-                    ),
-                title: Text(data.value[index].label),
+              itemBuilder: (context, index) => QRTile(
+                qrCode: data.value[index],
+                onDismiss: () => ref.read(codesDbProvider).removeEntry(data.value[index].id),
               ),
             ),
       orElse: () => const SizedBox.shrink(),
+    );
+  }
+}
+
+@visibleForTesting
+class QRTile extends StatelessWidget {
+  const QRTile({
+    super.key,
+    required this.qrCode,
+    required this.onDismiss,
+  });
+
+  final QRCode qrCode;
+  final VoidCallback onDismiss;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: Key('qrCode-${qrCode.id}'),
+      background: const ColoredBox(
+        color: Colors.red,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) => onDismiss(),
+      child: ListTile(
+        leading: QrImageView(
+            data: qrCode.data,
+            version: QrVersions.auto,
+            size: 48,
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            eyeStyle: QrEyeStyle(
+              eyeShape: QrEyeShape.square,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            dataModuleStyle: QrDataModuleStyle(
+              dataModuleShape: QrDataModuleShape.square,
+              color: Theme.of(context).colorScheme.onSurface,
+            )
+            // errorCorrectionLevel: QrErrorCorrectLevel.H,
+            ),
+        title: Text(qrCode.label),
+      ),
     );
   }
 }
